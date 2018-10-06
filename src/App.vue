0758fbab575c1799c40a2bf79ff314ce8df49b8a
aiyaya1993 <15958116062@163.com>
2018-10-06 11:20:42 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <!--<div id="app">-->
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+  <!--</div>-->
+  <!--<div id="drawing">-->
+      <!--<div class="circle" style=""></div>-->
+  <!--</div>-->
+  <div>
+    <canvas id="myCanvas" width="300" height="150" style="border:1px solid #d3d3d3;">
+      Your browser does not support the HTML5 canvas tag.
+    </canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted () {
+    var c = document.getElementById('myCanvas')
+    var ctx = c.getContext('2d')
+    ctx.beginPath()
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI)
+    ctx.stroke()
+  }
+}
+</script>
+
+<style>
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
+  /*.circle {*/
+    /*width: 350px;*/
+    /*height: 350px;*/
+    /*border-radius: 175px;*/
+    /*top: 100px;*/
+    /*left:-175px;*/
+    /*margin-left:50%;*/
+    /*background-color:red;*/
+    /*position:absolute;*/
+  /*}*/
+</style>
