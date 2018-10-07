c1873804bf1b6e8d7be2bac794a1943ccc5d8ee1
aiyaya1993 <15958116062@163.com>
2018-10-07 16:12:11 +0800

    修改旋转逻辑 通过:style属性和函数改变旋转角度

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
   </div>-->
   <div id="app">
     <img src="./assets/rotate.jpeg" class="rotateImg"/>
-    <img src="./assets/指针.png" :class="flag?'rotateImg1':'rotateImg2'" @click="start"/>
+    <!--<img src="./assets/指针.png" :class="{'rotateImg1':flag1, 'rotateImg2':flag2, 'rotateImg3':flag}" @click="start"/>-->
+    <img src="./assets/指针.png" :style="{transform:rotate_angle_pointer,transition:rotate_transition_pointer}" @click="start" class="rotateImg3">
   </div>
   <!--<div>-->
     <!--<canvas id="myCanvas" width="300" height="150" style="border:1px solid #d3d3d3;">-->
@@ -17,15 +18,19 @@
 <script>
 export default {
   name: 'App',
-  data () {
+  data: function () {
     return {
-      flag: false
+      flag: false,
+      flag1: false,
+      flag2: true,
+      rotate_angle_pointer: 'rotate(7deg)',
+      rotate_transition_pointer: 'transform 2s ease-in-out'
     }
   },
   methods: {
     start: function () {
-      this.flag = !this.flag
-      console.log(this.flag)
+      this.rotate_angle_pointer = `rotate(${Math.random() * 360}deg)`
+      console.log(this.rotate_angle_pointer)
     }
   }
   // mounted () {
@@ -64,6 +69,14 @@ export default {
   animation:circleRoate 10s ;
   animation-timing-function:linear;
 }
+.rotateImg3 {
+  position: relative;
+  top: -280px;
+  left: 150px;
+  width: 100px;
+  /*animation:circleRoate1 5s ;*/
+  /*animation-timing-function:linear;*/
+}
 .rotateImg2 {
   position: relative;
   top: -280px;
@@ -73,6 +86,10 @@ export default {
 @keyframes circleRoate{
   from{transform: rotate(0deg) infinite;}
   to{transform: rotate(720deg);}
+}
+@keyframes circleRoate1 {
+  from{transform: rotate(0deg) infinite;}
+  to{transform: rotate(200deg);}
 }
 
 </style>
